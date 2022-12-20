4276113b0c996295d9aa3435b3ae9fb219e948bd
Ahmet Galip <76816828+teknolojihamlesi@users.noreply.github.com>
2022-12-20 12:03:50 +0300

    Automatic Mail Sender
    Automatic mail sender repository

---
+++ OtomatikMail/OtomatikMail/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtomatikMail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+            string sql = "select * from orders where OrderDate <= DATEADD(DAY,-1080,convert(date,sysdatetime()))";
+            SqlDataAdapter sda = new SqlDataAdapter(sql,cs);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            //Customer ID , OrderDate olacak
+            string mailBody = "";
+            foreach (DataRow herhangi in dt.Rows)
+            {
+                mailBody += herhangi["OrderDate"] + " " + herhangi["CustomerID"];
+            }
+            MailGonder(mailBody);
+        }
+
+        private static void MailGonder(string mailBody)
+        {
+            MailMessage ePosta = new MailMessage();
+            ePosta.From = new MailAddress("mailadresim@gmail.com"); // Kimden gidecek
+            ePosta.To.Add("gonderilecekmailadresi@gmail.com");
+            ePosta.Subject = "Son Siparişler";
+            ePosta.Body = mailBody;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new System.Net.NetworkCredential("mailadresim@gmail.com","mailsifrem");
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.Send(ePosta);
+        }
+    }
+}
